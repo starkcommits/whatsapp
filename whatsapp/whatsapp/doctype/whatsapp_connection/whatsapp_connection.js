a260828a29aa2478053ba2d1d841a71f85f888d7
starkcommits <tinkal.kumar@xettle.net>
2025-11-22 20:18:00 +0000

    fix doctypes

---
+++ whatsapp/whatsapp/doctype/whatsapp_connection/whatsapp_connection.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, INIA GLOBAL and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("WhatsApp Connection", {
+// 	refresh(frm) {
+
+// 	},
+// });
